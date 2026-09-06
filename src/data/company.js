0e40b00ebdb241d7e8c0a0cf5cb7b8a664311c16
njrjr100-crypto <njrjr100@gmail.com>
2026-09-06 15:57:43 +0530

    Update company phone and WhatsApp number

--- src/data/company.js
+++ src/data/company.js
@@ -5,10 +5,10 @@
 export const company = {
   name: "TAHFEEL TECHNICAL SERVICE",
   tagline: "Integrated Technical & Contracting Solutions",
-  phone: "+971 50 958 4547",
-  phoneHref: "tel:+971509584547",
-  whatsappNumber: "+971 50 958 4547",
-  whatsappHref: "https://wa.me/971509584547",
+  phone: "+971 50 964 1429",
+  phoneHref: "tel:+971509641429",
+  whatsappNumber: "+971 50 964 1429",
+  whatsappHref: "https://wa.me/971509641429",
   email: "info@tahfeel.com",
   location: "United Arab Emirates",
   description:
